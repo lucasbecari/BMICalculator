5a79e0d02384bae19ec9d544827ead5db6654bee
Lucas Becari <lucasbecari@gmail.com>
2025-10-15 23:03:59 -0300

    First commit, add all archives

---
+++ BMICalculator/BMICalculator/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.Models
+{
+    public class Person
+    {
+        public double Weight { get; set; }
+        public double Height { get; set; }
+
+        public double CalculateBMI()
+        {
+            return Weight / (Weight * Height);
+        }
+    }
+
+}
---
+++ BMICalculator/BMICalculator/ViewModels/PersonViewModel.cs
@@ -0,0 +1,56 @@
+﻿using BMICalculator.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICalculator.ViewModels
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person person = new Person();
+
+        public double Weight
+        {
+            get => person.Weight;
+            set
+            {
+                person.Weight = value;
+                OnPropertyChanged(nameof(Weight));
+            }
+        }
+
+        public double Height
+        {
+            get => person.Height;
+            set
+            {
+                person.Height = value;
+                OnPropertyChanged(nameof(Height));
+            }
+        }
+
+        public double BMI
+        {
+            get => person.CalculateBMI();
+            set
+            {
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string Name = null)
+        {
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(Name));
+        }
+    }
+}
+
+
+
+
